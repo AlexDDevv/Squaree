bc3cb6005df5f0f92ad3c45e6019e21fdb8dd095
Alexis <AlexDdevs@gmail.com>
2024-12-08 18:59:16 +0100

    💩 Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import Header from "./components/Header";
 import SectionSquares from "./components/squares/SectionSquares";
 import Marquee from "./components/UI/Marquee";
@@ -10,8 +12,28 @@ import pdp from "../assets/images/userSquaree.svg";
 import Link from "next/link";
 import SquareTool from "./components/SquareTool";
 import Testimonials from "./components/Testimonials";
+import PricingPlan from "./components/PricingPlan";
+import Footer from "./components/Footer";
 
 export default function Home() {
+    const [monthly, setMonthly] = useState(true);
+    const [yearly, setYearly] = useState(false);
+
+    const toggleSubscription = () => {
+        setMonthly(!monthly);
+        setYearly(!yearly);
+    };
+
+    const toggleMonthly = () => {
+        setMonthly(true);
+        setYearly(false);
+    };
+
+    const toggleYearly = () => {
+        setYearly(true);
+        setMonthly(false);
+    };
+
     return (
         <>
             <Header />
@@ -197,6 +219,58 @@ export default function Home() {
                     ))}
                 </div>
             </section>
+            <section className="flex flex-col items-center justify-center mb-28">
+                <div className="flex items-center justify-center gap-5 mb-20">
+                    <h2
+                        className={`font-dmSans ${
+                            monthly ? "font-bold text-5xl" : "text-xl"
+                        } text-text transition-all duration-200 ease-in-out cursor-pointer`}
+                        onClick={toggleMonthly}
+                    >
+                        Billed Monthly
+                    </h2>
+                    <div className="flex items-center justify-center gap-5">
+                        <div
+                            className="bg-bgBtn rounded-full w-20 h-11   flex items-center cursor-pointer relative"
+                            onClick={toggleSubscription}
+                        >
+                            <div
+                                className={`bg-white h-9 w-9 rounded-full absolute left-1 transition-transform duration-200 ease-in-out transform ${
+                                    yearly
+                                        ? "translate-x-full"
+                                        : "translate-x-0"
+                                }`}
+                            ></div>
+                        </div>
+                        <p
+                            className={`font-manrope ${
+                                yearly ? "font-bold text-5xl" : "text-xl"
+                            } text-text transition-all duration-200 ease-in-out cursor-pointer`}
+                            onClick={toggleYearly}
+                        >
+                            Billed Yearly (save 15%)
+                        </p>
+                    </div>
+                </div>
+                <div className="flex items-start justify-center gap-10">
+                    {data.pricing.map((item) => (
+                        <PricingPlan
+                            key={item.subscription}
+                            subscription={item.subscription}
+                            price={item.price}
+                            priceMonth={item.priceMonth}
+                            priceYear={item.priceYear}
+                            perMonth={item.monthly}
+                            perYear={item.yearly}
+                            yearly={yearly}
+                            description={item.description}
+                            advantages={item.advantages}
+                            CTA={item.CTA}
+                        />
+                    ))}
+                </div>
+            </section>
+            <Footer />
         </>
     );
 }

fa108042c64b4f5b7874410f4f4c351fd2e8354d
Alexis <AlexDdevs@gmail.com>
2024-12-14 08:59:57 +0100

    🎨 Create and animate MarqueeSection

---
+++ src/app/components/MarqueeSection.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Marquee from "./UI/Marquee";
+import { motion } from "framer-motion";
+
+const marqueeAnimation = {
+    initial: { opacity: 0, scale: 0.8 },
+    animate: { opacity: 1, scale: 1 },
+    exit: { opacity: 1, scale: 1 },
+    transition: {
+        opacity: {
+            duration: 0.4,
+            ease: "easeInOut",
+        },
+        scale: {
+            duration: 0.5,
+            ease: "easeOut",
+            delay: 0.1,
+        },
+    },
+};
+
+export default function MarqueeSection() {
+    return (
+        <motion.section
+            className="max-w-[800px] mx-auto mb-36 relative"
+            initial={marqueeAnimation.initial}
+            whileInView={marqueeAnimation.animate}
+            exit={marqueeAnimation.exit}
+            transition={marqueeAnimation.transition}
+            viewport={{ once: true, amount: 0.5 }}
+        >
+            <p className="font-dmSans font-medium text-xl text-text text-center opacity-50 mb-10">
+                We have the fast paced growing companies with us
+            </p>
+            <Marquee />
+            <div className="z-2 w-full h-full absolute inset-0 custom-gradient"></div>
+        </motion.section>
+    );
+}

5301b673195e94bffb591d9d03cdee188f058295
Alexis <AlexDdevs@gmail.com>
2024-12-15 11:18:20 +0100

    🎨 Call the new component for button

--- src/app/components/ScaleSection.tsx
+++ src/app/components/ScaleSection.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import data from "../data/data.json";
 import NumberGrowth from "../components/UI/NumberGrowth";
 import GrowthCurve from "../components/UI/GrowthCurve";
-import { ArrowRight } from "lucide-react";
+import ExploreBtn from "./UI/ExploreBtn";
 import { motion } from "framer-motion";
 
 const scaleAnimation = {
@@ -66,40 +66,7 @@ export default function ScaleSection() {
                 ))}
             </div>
             <div className="flex items-center justify-center h-14">
-                <motion.button
-                    className={`rounded-full flex items-center justify-center gap-1 w-0 h-full transition-all duration-500 ease-in-out ${
-                        isHover ? "w-[200px] bg-bgBtn" : ""
-                    }`}
-                    initial={{ opacity: 0 }}
-                    whileInView={{ opacity: 1 }}
-                    exit={{ opacity: 1 }}
-                    transition={{ duration: 0.2, ease: "easeInOut" }}
-                    viewport={{ once: true, amount: 0.5 }}
-                >
-                    <motion.span
-                        className=" left-14 font-inter font-semibold text-white"
-                        initial={{ opacity: 0, x: 20 }}
-                        animate={{
-                            opacity: isHover ? 1 : 0,
-                            x: isHover ? 0 : 20,
-                        }}
-                        transition={{
-                            duration: 0.3,
-                            ease: "easeInOut",
-                            delay: isHover ? 0.25 : 0,
-                        }}
-                    >
-                        Explore our way
-                    </motion.span>
-                    <div
-                        className={`bg-bgBtn rounded-full flex items-center justify-center p-5 cursor-pointer transition-all duration-500 ease-in-out ${
-                            isHover ? "p-0" : ""
-                        }`}
-                        onMouseEnter={handleHover}
-                    >
-                        <ArrowRight className="text-white h-4 w-4" />
-                    </div>
-                </motion.button>
+                <ExploreBtn isHover={isHover} handleHover={handleHover} />
             </div>
         </section>
     );
